99afd11aad07fcb90e59aa7a4b2669f0505dee61
33QWERTY33 <120336644+33QWERTY33@users.noreply.github.com>
2024-04-09 17:18:04 -0400

    got data displaying on web page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = ' pokemondb.cl06m4ie8jy6.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_HOST'] = 'pokemondb.cl06m4ie8jy6.us-east-2.rds.amazonaws.com'
 app.config['MYSQL_USER'] = 'admin'
 app.config['MYSQL_PASSWORD'] = 'NinjaSexParty'
 app.config['MYSQL_DB'] = 'PokemonDB'
@@ -15,22 +15,10 @@ mysql = MySQL(app)
 @app.route('/')
 def home():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM pokemon")
+    cur.execute("SELECT * FROM Pokemon")
     pokemon = cur.fetchall()
     cur.close()
     return render_template('index.html', pokemon=pokemon)
-
-# Add new Pokémon route
-@app.route('/add_pokemon', methods=['POST'])
-def add_pokemon():
-    if request.method == 'POST':
-        name = request.form['name']
-        type = request.form['type']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO pokemon (name, type) VALUES (%s, %s)", (name, type))
-        mysql.connection.commit()
-        cur.close()
-        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)

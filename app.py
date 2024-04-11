8b5d514e7167a843b38a6d519a2a960338fbca9e
33QWERTY33 <120336644+33QWERTY33@users.noreply.github.com>
2024-04-10 20:28:41 -0400

    created home page and abilities list page

--- app.py
+++ app.py
@@ -14,11 +14,16 @@ mysql = MySQL(app)
 # Home route
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+# pokemon list
+@app.route('/pokemon_list')
+def pokemon_list():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM Pokemon")
     pokemon = cur.fetchall()
     cur.close()
-    return render_template('index.html', pokemon=pokemon)
+    return render_template('pokemon_list.html', pokemon=pokemon)
 
 
 # typings list
@@ -29,6 +34,15 @@ def typings_list():
     typings = cur.fetchall()
     cur.close()
     return render_template('typings_list.html', typings=typings)
+
+# abilities list
+@app.route('/abilities_list')
+def abilities_list():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM Abilities")
+    abilities = cur.fetchall()
+    cur.close()
+    return render_template('abilities_list.html', abilities=abilities)
 
 # query DB
 @app.route('/query')
@@ -42,12 +56,22 @@ def results():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
 
-        results = cur.execute(f"SELECT * FROM Pokemon WHERE NationalPokedexNum = '{request.form.get("NationalPokedexNum")}'")
+        NationalPokedexNum = request.form.get("NationalPokedexNum")
+        Pokemon = request.form.get("Pokemon").capitalize()
+        Typing = request.form.get("Typing")
+        Abilities = request.form.get("Abilities")
+        BST = request.form.get("Base Stat Total")
+
+        print("\n-----------------------",request.form.keys(),"----------------------\n")
+
+        results = cur.execute(f"""SELECT * FROM Pokemon WHERE 
+                                {"NationalPokedexNum = '" + NationalPokedexNum + "'" if NationalPokedexNum else ''}
+                                {"Pokemon = '" + Pokemon + "'" if Pokemon else ''}
+                                {"Typing LIKE '" + Typing + "'" if Typing else ''}
+                                {"Abilities LIKE '" + Abilities + "'" if Abilities else ''}
+                                {"BaseStatTotal >= " + BST if BST != 0 else ''}""")
         results = cur.fetchall()
         cur.close()
-        print("------------------------------------START DEBUG INFO------------------------------------")
-        print("QUERY RESULTS >> " + str(results))
-        print("------------------------------------END DEBUG INFO------------------------------------")
         return render_template('successful_query.html', results = results)
     else:
        return render_template('unsuccessful_query.html', results = request.form)
